e5c35f0f51dea63dc73a7db76bd5e8d93805d473
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-10-30 21:54:04 +0600

    added useState

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { apiRequest } from "../utils/apiRequest";
 
 export default function Home() {
+    const [errData, errDataSet] = useState("");
     useEffect(() => {
         const fetchData = async () => {
             const data = await apiRequest("http://localhost:5000", "GET");
-            console.log("hi", data);
+            errDataSet(data.message);
         };
         fetchData();
-    });
+    }, []);
     return (
         <div>
             <Head>
                 <title>Less Async</title>
             </Head>
             <h1> Test frontend error handling</h1>
+            <h2>{errData}</h2>
         </div>
     );
 }
