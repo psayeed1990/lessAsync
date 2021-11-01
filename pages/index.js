fb0e7afb47082d6c9038f27386ca41eda0f08f10
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-11-01 18:27:57 +0600

    remove http from internal link

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ export default function Home() {
     const [errData, errDataSet] = useState("");
     useEffect(() => {
         const fetchData = async () => {
-            const data = await apiRequest(
-                "http://srv-captain--baqiudfjth",
-                "GET"
-            );
+            const data = await apiRequest("srv-captain--baqiudfjth", "GET");
             errDataSet(data.message);
         };
         fetchData();
